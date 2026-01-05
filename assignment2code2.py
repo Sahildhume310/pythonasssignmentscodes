25ae6c68de2a77b13a3171457eed467e500f49c4
Sahildhume310 <sahildhume310@gmail.com>
2026-01-05 21:30:05 +0530

    Add files via upload

---
+++ assignment2code2.py
@@ -0,0 +1,16 @@
+print("enter first numbers")
+number1=int(input())
+print("enter second numbers")
+number2=int(input())
+
+add=number1+number2
+print("addition is :",add)
+
+sub=number1-number2
+print("subtraction  is :",sub)
+
+mult=number1*number2
+print("multiplication is :",mult)
+
+div=number1/number2
+print("division is :",div)
