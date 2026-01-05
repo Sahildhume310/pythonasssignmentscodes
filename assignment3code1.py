d5a37e53ae2a13ad808cd7f6a027c70c33fb6b29
Sahildhume310 <sahildhume310@gmail.com>
2026-01-05 21:30:43 +0530

    Add files via upload

---
+++ assignment3code1.py
@@ -0,0 +1,10 @@
+def mult(no1,no2):
+    Ans=0
+    Ans=no1*no2
+    return Ans
+print("enter first number:")
+no1=int(input())
+print("enter second number:")
+no2=int(input())
+Result=mult(no1,no2)
+print("multiplication is:",Result)
